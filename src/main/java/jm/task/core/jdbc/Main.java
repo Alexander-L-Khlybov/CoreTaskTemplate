a5f388b76abdb76aa568419ad2b570724465a7b4
Aleksandr Khlybov <alexander.l.khlybov@gmail.com>
2021-11-15 00:43:52 +0300

    getConnection

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        try {
+            System.out.println(Util.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
